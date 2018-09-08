b6fd4181d6198666e6a3dbdf094c4f228bada276
William Ku <kucheech@gmail.com>
2018-09-08 20:10:23 +0800

    Add project files.

---
+++ Scenario 1-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Scenario_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n: ");
+            int n = int.Parse(Console.ReadLine());
+            int fact = 1;
+
+            while(n > 1)
+            {
+                fact *= n;
+                n--;
+            }
+
+            Console.WriteLine("fact = {0}", fact);
+        }
+    }
+}
